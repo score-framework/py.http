f08a0f4ca0884f062698327698549d87fffb3ddb
Necdet Can Ateşman <can@atesman.at>
2019-12-12 14:20:20 +0100

    Release: 0.5.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.http',
-    version='0.5.2',
+    version='0.5.3',
     description='HTTP handler of The SCORE Framework',
     long_description=README,
     author='strg.at',
